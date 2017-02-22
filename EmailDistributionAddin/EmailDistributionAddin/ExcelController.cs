fbd7eea49d6046b4b515e3737023dea000e4a841
Andrew Sprague <Andrew Sprague>
2017-02-22 20:51:40 +0000

    Move HelloWorld tutorial to sub directory, copy project

---
+++ EmailDistributionAddin/EmailDistributionAddin/ExcelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Application = NetOffice.ExcelApi.Application;
+using ExcelDna.Integration.CustomUI;
+using NetOffice.ExcelApi;
+
+namespace EmailDistributionAddin
+{
+    class ExcelController : IDisposable
+    {
+        private readonly IRibbonUI _modelingRibbon;
+        protected readonly Application _excel;
+
+        public ExcelController(Application excel, IRibbonUI modelingRibbon)
+        {
+            _modelingRibbon = modelingRibbon;
+            _excel = excel;
+        }
+
+        public void PressMe()
+        {
+            var activeSheet = _excel.ActiveSheet as Worksheet;
+            activeSheet.Range("A1").Value = "Hello, World!";
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
