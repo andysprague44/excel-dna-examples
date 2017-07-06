e54eeb662fe889076d5cd4a06ab19a623fdf9707
Andy Sprague <andy.sprague44@gmail.com>
2017-07-06 23:59:48 +0100

    VB.NET hello world custom ribbon example

---
+++ HelloWorld_VB.Net/MyExcelAddIn/CustomRibbon.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.IO
+Imports System.Resources
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports Application = NetOffice.ExcelApi.Application
+Imports ExcelDna.Integration.CustomUI
+
+<ComVisible(True)> _ 
+Public Class CustomRibbon : Inherits ExcelRibbon
+    
+    private  _excel As Application
+    private  _thisRibbon As IRibbonUI
+
+    Overrides Function GetCustomUI(ribbonId As String) As String
+        _excel = new Application(Nothing, ExcelDna.Integration.ExcelDnaUtil.Application)
+        Dim ribbonXml As String = GetCustomRibbonXML()
+        return ribbonXml        
+    End Function
+
+    Private Function GetCustomRibbonXML() As String
+        Dim ribbonXml As String = Nothing
+        Dim thisAssembly As Assembly = GetType(CustomRibbon).Assembly
+        Dim resourceName As String = GetType(CustomRibbon).Namespace + ".CustomRibbon.xml"
+
+        Using stream as Stream = thisAssembly.GetManifestResourceStream(resourceName)
+        Using reader As StreamReader = new StreamReader(stream)
+            ribbonXml = reader.ReadToEnd()                
+        End Using
+        End Using
+
+        If (ribbonXml Is Nothing) Then
+            throw New MissingManifestResourceException(resourceName)
+        End If
+        Return ribbonXml
+    End Function
+
+    Public Sub OnLoad(ribbon As IRibbonUI)
+        _thisRibbon = ribbon
+        If (_excel.ActiveWorkbook Is Nothing) Then
+            _excel.Workbooks.Add()
+        End If
+    End Sub
+
+    Public Sub OnPressMe(control As IRibbonControl)
+            Using controller As ExcelController = New ExcelController(_excel, _thisRibbon)
+                controller.PressMe()            
+            End Using
+    End Sub
+
+End Class
---
+++ HelloWorld_VB.Net/MyExcelAddIn/ExcelController.vb
@@ -0,0 +1,57 @@
+﻿Imports System
+Imports Application = NetOffice.ExcelApi.Application
+Imports ExcelDna.Integration.CustomUI
+Imports NetOffice.ExcelApi
+
+
+Public Class ExcelController : Implements IDisposable
+    private readonly _modelingRibbon As IRibbonUI
+    protected readonly _excel As Application
+
+    public Sub New(excel As Application, modelingRibbon As IRibbonUI)
+        _modelingRibbon = modelingRibbon
+        _excel = excel
+    End Sub
+
+    public Sub PressMe()
+        Dim activeSheet As Worksheet = CType(_excel.ActiveSheet, Worksheet)
+        activeSheet.Range("A1").Value = "Hello, World!"
+    End Sub
+
+
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+
+End Class
+
